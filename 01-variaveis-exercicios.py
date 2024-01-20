daed043792b1da6071d56683cd029c229d30a2b4
Henrique <henrique.ajnbranco@gmail.com>
2024-01-20 15:00:57 -0300

    2024-20-01: adicionado arquivos da aula

---
+++ 01-variaveis-exercicios.py
@@ -0,0 +1,8 @@
+# Calcula a área (em m²) de uma sala com dimensões 
+# 3m (largura) x 4m (comprimento). Imprima o resultado na tela.
+# Escrever o código abaixo
+
+
+# Calcula o volume (em litros) de uma piscina com dimensões 
+# 4m (largura) x 5m (comprimento) x 2m (profundidade). Imprima o resultado na tela
+# Escrever o código abaixo
---
+++ 01-variaveis.py
@@ -0,0 +1,55 @@
+# # Variáveis
+# nome = "Henrique"
+# print(nome)
+# print(type(nome))
+
+# # Tipos de variáveis
+# exercicio = "2.0"
+# print(type(exercicio))
+
+# # Definição de nomes de variáveis
+# # O que não pode!
+
+# # Não pode conter espaço
+# meu nome = "Henrique"
+
+# # Não pode conter hifen
+# meu-nome = "Henrique"
+
+# # Não posso começar variáveis com números
+# 1nome = "Henrique"
+# 2nome = "Karen"
+
+# # Não posso usar palavras reservadas (comandos)
+# for = 6
+
+# # O que eu posso?
+# # Único caracter especial é o underline
+# # Letras + underline
+# # É possível começar com underline
+_meu_nome2 = "Henrique"
+print(_meu_nome2)
+
+# # Convenções
+# PascalCase = "Todas as primeiras letras são maiúsculas"
+# camelCase = "Todas as primeiras letras são maiúsculas exceto a primeira!"
+sneak_case = "Todas letras minúsculas separadas por underline"
+
+# # Boas práticas na nomeação de variáveis
+# # Nomes significativos!!!
+x = ""
+y = 32
+z = "Alves Junqueira Nogueira Branco"
+
+nome = ""
+idade_em_meses = ...
+sobrenome = ""
+
+valor_reais_vendas_por_mes = 120
+vendas_mes = 120
+vm = 120
+
+# # Tipos de variáveis na criação
+nome: str = "Henrique"
+idade_em_meses: int = "32"
+sobrenome: str = "Branco"
